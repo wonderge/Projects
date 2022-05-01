011b7862745ab602d5eabddf64cb1c9e8c50f57c
wonderge <wonderge@yahoo.ca>
2022-05-01 10:03:20 -0400

    PWA setup

--- fabric_calculation_pwa/pages/_app.tsx
+++ fabric_calculation_pwa/pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png" />
+        <meta name="theme-color" content="#fff" />
+        <title>FabricCalculation</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
