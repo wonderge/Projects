dd30c6014b911b2623f1dbef17f2154b8e438fb5
wonderge <wonderge@yahoo.ca>
2022-10-28 11:42:58 -0400

    i18n for flower implemented

--- fabric_calculation_pwa/pages/flower.tsx
+++ fabric_calculation_pwa/pages/flower.tsx
@@ -1,72 +1,106 @@
 import type { NextPage } from 'next'
-import { Container, Card, Form, Row, Col, Button } from 'react-bootstrap'
+import { useRouter } from 'next/router'
+import { ChangeEvent, FormEvent, useRef, useState } from 'react'
+import { Container, Card, Form, Row, Col, Button, FormControlProps, FormControl } from 'react-bootstrap'
+import fetchApi from '../utils/helpers/fetchApi'
+import getLabels from '../utils/i18n/labels'
 
 const Flower: NextPage = () => {
+  const [amount, setAmount] = useState<number[]>(Array(9).fill(0));
+  const [length, setLength] = useState(0);
+  const [width, setWidth] = useState(0);
+  const [result, setResult] = useState('');
+  const form = useRef<HTMLFormElement>(null);
+  const { Length, Width, Calculate, Clear, Flower } = getLabels(useRouter().locale);
+
+  const calculate = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const res = await fetchApi('/api/flower', { amount, length, width });
+    if (res.message) {
+      setResult(res.message);
+    } else {
+      setResult(`${res.required}pcs`)
+    }
+  }
+
+  const clear = () => {
+    form.current!.reset();
+    setAmount(Array(9).fill(0));
+    setLength(0);
+    setWidth(0);
+    setResult('');
+  }
+
+  const updateAmountAt = (value: number, index: number) => {
+    setAmount(amount.map((e, i) => i == index ? value : e))
+  }
+
   return (
     <Container className='d-flex align-items-center justify-content-center flex-column' style={{ minHeight: "calc(100vh - 56px)" }}>
       <div className="w-100" style={{ maxWidth: '400px' }}>
         <Card className='p-1'>
           <Card.Body>
-            <h2 className='text-center'>Flower</h2>
-            <Form>
+            <h2 className='text-center'>{Flower}</h2>
+            <Form onSubmit={calculate} ref={form}>
               <Row>
                 <Form.Group as={Col} className="mb-3" controlId="80cm">
                   <Form.Label>80cm</Form.Label>
-                  <Form.Control type="number" className='mr-3 pr-3' />
+                  <Form.Control type="number" className='mr-3 pr-3' onChange={(e) => updateAmountAt(+e.target.value, 0)} />
                 </Form.Group>
                 <Form.Group as={Col} className="mb-3" controlId="70cm">
                   <Form.Label>70cm</Form.Label>
-                  <Form.Control type="number" />
+                  <Form.Control type="number" onChange={(e) => updateAmountAt(+e.target.value, 1)} />
                 </Form.Group>
               </Row>
               <Row>
                 <Form.Group as={Col} className="mb-3" controlId="60cm">
                   <Form.Label>60cm</Form.Label>
-                  <Form.Control type="number" className='mr-3 pr-3' />
+                  <Form.Control type="number" className='mr-3 pr-3' onChange={(e) => updateAmountAt(+e.target.value, 2)} />
                 </Form.Group>
                 <Form.Group as={Col} className="mb-3" controlId="50cm">
                   <Form.Label>50cm</Form.Label>
-                  <Form.Control type="number" />
+                  <Form.Control type="number" onChange={(e) => updateAmountAt(+e.target.value, 3)} />
                 </Form.Group>
               </Row>
               <Row>
                 <Form.Group as={Col} className="mb-3" controlId="40cm">
                   <Form.Label>40cm</Form.Label>
-                  <Form.Control type="number" className='mr-3 pr-3' />
+                  <Form.Control type="number" className='mr-3 pr-3' onChange={(e) => updateAmountAt(+e.target.value, 4)} />
                 </Form.Group>
                 <Form.Group as={Col} className="mb-3" controlId="30cm">
                   <Form.Label>30cm</Form.Label>
-                  <Form.Control type="number" />
+                  <Form.Control type="number" onChange={(e) => updateAmountAt(+e.target.value, 5)} />
                 </Form.Group>
               </Row>
               <Row>
                 <Form.Group as={Col} className="mb-3" controlId="20cm">
                   <Form.Label>20cm</Form.Label>
-                  <Form.Control type="number" className='mr-3 pr-3' />
+                  <Form.Control type="number" className='mr-3 pr-3' onChange={(e) => updateAmountAt(+e.target.value, 6)} />
                 </Form.Group>
                 <Form.Group as={Col} className="mb-3" controlId="15cm">
                   <Form.Label>15cm</Form.Label>
-                  <Form.Control type="number" />
+                  <Form.Control type="number" onChange={(e) => updateAmountAt(+e.target.value, 7)} />
                 </Form.Group>
               </Row>
               <div className='d-flex justify-content-center'>
                 <Form.Group className="mb-3" controlId="10cm">
                   <Form.Label>10cm</Form.Label>
-                  <Form.Control type="number" className='mr-3 pr-3' />
+                  <Form.Control type="number" className='mr-3 pr-3' onChange={(e) => updateAmountAt(+e.target.value, 8)} />
                 </Form.Group>
               </div>
               <Form.Group className="mb-3" controlId="length">
-                <Form.Label>Length</Form.Label>
-                <Form.Control type="number" className='mr-3 pr-3' />
+                <Form.Label>{Length}</Form.Label>
+                <Form.Control type="number" className='mr-3 pr-3' onChange={(e) => setLength(+e.target.value)} />
               </Form.Group>
               <Form.Group className="mb-3" controlId="width">
-                <Form.Label>Width</Form.Label>
-                <Form.Control type="number" className='mr-3 pr-3' />
+                <Form.Label>{Width}</Form.Label>
+                <Form.Control type="number" className='mr-3 pr-3' onChange={(e) => setWidth(+e.target.value)} />
               </Form.Group>
               <Row>
-                <Col className='text-center'><Button>Calculate</Button></Col>
-                <Col className='text-center'><Button>Clear</Button></Col>
+                <Col className='text-center'><Button type='submit'>{Calculate}</Button></Col>
+                <Col className='text-center'><Button onClick={clear}>{Clear}</Button></Col>
               </Row>
+              <div className='text-center'>{result}</div>
             </Form>
           </Card.Body>
         </Card>
--- fabric_calculation_pwa/utils/helpers/fetchApi.ts
+++ fabric_calculation_pwa/utils/helpers/fetchApi.ts
@@ -1,6 +1,6 @@
 import ResModel from "../../models/ResModel";
 
-const fetchApi = async (endpoint: string, payload: { [key: string]: number | string }): Promise<ResModel> => {
+const fetchApi = async (endpoint: string, payload: { [key: string]: number | string | Array<number> }): Promise<ResModel> => {
   const body = JSON.stringify(payload);
   const res: Response = await fetch(endpoint, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body });
   return await res.json();
