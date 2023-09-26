3be293686dd2447ddb0dcdd24fa4a6063f9976ec
wonderge <wonderge@yahoo.ca>
2023-09-26 11:54:08 -0400

    async loading of bootstrap script

--- fabric_calculation_pwa/app/[locale]/layout.tsx
+++ fabric_calculation_pwa/app/[locale]/layout.tsx
@@ -31,7 +31,7 @@ const RootLayout = async ({ children, params: { locale } }: { children: ReactNod
         <NextIntlClientProvider locale={locale} messages={labels}>
           {children}
         </NextIntlClientProvider>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossOrigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossOrigin="anonymous" async></script>
       </body>
     </html>
   )
