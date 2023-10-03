3a2d224e2a67165f68a7af0e98775db138937b2d
wonderge <52543871+wonderge@users.noreply.github.com>
2023-10-03 10:49:07 -0400

    Delete next-chat project

--- next-chat/lib/session.ts
+++
@@ -1,7 +0,0 @@
-import nextSession from 'next-session';
-import { promisifyStore } from 'next-session/lib/compat'
-import MongoStore from 'connect-mongo';
-
-const store = promisifyStore(MongoStore.create({ mongoUrl: process.env.MONGO_URI, ttl: 60 * 60 * 24 * 7 }));
-
-export const getSession = nextSession({ store, cookie: { path: '/', maxAge: 1000 * 60 * 60 * 24 * 7 } })
