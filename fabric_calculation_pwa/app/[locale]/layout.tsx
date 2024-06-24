0daf43c91219ed60c01d1918679eb2e5831654d3
wonderge <wonderge@yahoo.ca>
2024-06-24 15:47:46 -0400

    i18n fix

--- fabric_calculation_pwa/app/[locale]/layout.tsx
+++ fabric_calculation_pwa/app/[locale]/layout.tsx
@@ -1,7 +1,7 @@
 import { Metadata, Viewport } from "next"
 import { ReactNode } from "react"
 import { NextIntlClientProvider } from "next-intl"
-import { notFound } from 'next/navigation'
+import { getLocale, getMessages } from "next-intl/server"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../styles/globals.css';
 
@@ -21,17 +21,13 @@ export function generateStaticParams() {
   return [{ locale: 'en' }, { locale: 'zh' }];
 }
 
-const RootLayout = async ({ children, params: { locale } }: { children: ReactNode, params: { locale: string } }) => {
-  let labels
-  try {
-    labels = (await import(`../../labels/${locale}.json`)).default
-  } catch (error) {
-    notFound()
-  }
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+  const locale = await getLocale()
+  const messages = await getMessages()
   return (
     <html lang={locale} translate="no" className="notranslate">
       <body className="gray">
-        <NextIntlClientProvider locale={locale} messages={labels}>
+        <NextIntlClientProvider locale={locale} messages={messages}>
           {children}
         </NextIntlClientProvider>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossOrigin="anonymous" async></script>
